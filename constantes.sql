2eb4a11ee50c9442da858715efa16da25a4a0cbc
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-07-16 11:58:44 -0300

    testes finalizados de constantes

--- constantes.sql
+++ constantes.sql
@@ -1,2 +1,23 @@
 DECLARE
-    
+    vPi CONSTANT NUMBER(38, 15) := 3.141592653589793;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Pi = ' || vPi);
+END;
+
+-- testando o erro mudando o valor de vPi
+DECLARE
+    vPi CONSTANT NUMBER(38, 15) := 3.141592653589793;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Pi = ' || vPi);
+    vPi := 3.14159265;
+    DBMS_OUTPUT.PUT_LINE('Pi = ' || vPi);
+END;
+
+
+DECLARE
+    vTextoFixo CONSTANT CHAR(2) := 'PB';
+    vTextoVariavel CONSTANT VARCHAR2(100) := 'Guarabira, PB';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Texto Fixo: ' || vTextoFixo);
+    DBMS_OUTPUT.PUT_LINE('Texto Variavel: ' || vTextoVariavel);
+END;
