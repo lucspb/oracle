fd77f99ca289efc52e402149453dc2dc7c618c01
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-07-24 22:24:46 -0300

    comando delete no PL/SQL

---
+++ comandosSql/comandoDelete.sql
@@ -0,0 +1,10 @@
+--utilizando o comando DELETE no PL/SQL
+
+SET SERVEROUTPUT ON
+DECLARE
+    vEmployee_id     employees.employee_id%type := 208;
+BEGIN
+    DELETE FROM employees
+    WHERE   employee_id = vEmployee_id;
+    COMMIT;
+END;
