f6ca794dbb90b7c7fdd9ab967d94fd9532c47f61
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-07-24 22:21:16 -0300

    comando update no PL/SQL

---
+++ comandosSql/comandoUpdate.sql
@@ -0,0 +1,13 @@
+--utilizando o comando UPDATE no PL/SQL
+
+SET SERVEROUTPUT ON
+DECLARE
+    vEmployee_id    employees.employee_id%type := 208;
+    vPercentual     NUMBER(3) := 10;
+BEGIN
+    UPDATE      employees
+    SET         salary = salary * (3 + vPercentual / 100)
+    WHERE       employee_id = vEmployee_id;
+    COMMIT;
+END;
+
