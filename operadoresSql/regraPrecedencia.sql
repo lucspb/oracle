a2f47c5a965a7df1900192908c38372e9eebf7d8
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-08-06 13:00:29 -0300

    regras de precedencia logica

---
+++ operadoresSql/regraPrecedencia.sql
@@ -0,0 +1,27 @@
+-- OPERADORES PL/SQL (regras de precedencia)
+
+SET SERVEROUTPUT ON
+DECLARE
+    vNota1 NUMBER(11,2) := 7.0;
+    vNota2 NUMBER(11,2) := 6.0;
+    vNota3 NUMBER(11,2) := 9.0;
+    vNota4 NUMBER(11,2) := 6.0;
+    vMedia NUMBER(11,2);
+BEGIN
+    vMedia := vNota1 + vNota2 + vNota3 + vNota4 / 4;
+    DBMS_OUTPUT.PUT_LINE('Media = ' || vMedia);
+END;
+
+-- corrigindo o calculo da media colocando os parenteses
+
+SET SERVEROUTPUT ON
+DECLARE
+    vNota1 NUMBER(11,2) := 7.0;
+    vNota2 NUMBER(11,2) := 6.0;
+    vNota3 NUMBER(11,2) := 9.0;
+    vNota4 NUMBER(11,2) := 6.0;
+    vMedia NUMBER(11,2);
+BEGIN
+    vMedia := (vNota1 + vNota2 + vNota3 + vNota4) / 4;
+    DBMS_OUTPUT.PUT_LINE('Media = ' || vMedia);
+END;
